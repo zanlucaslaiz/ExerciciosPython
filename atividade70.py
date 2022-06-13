f48dca01157ea496c90a00316d5946158931ce45
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2022-06-13 08:28:08 -0300

    Delete atividade70.py

--- atividade70.py
+++
@@ -1,1 +0,0 @@
-# Desafio 66 - Crie um programa que leia vários números inteiros pelo teclado. O programa só vai para quando o usuario digitar o valor 999, que é a condição de parada. No final, mostre quantos numeros foram digitados e qual foi a soma entre eles (desconsiderando o flag).
