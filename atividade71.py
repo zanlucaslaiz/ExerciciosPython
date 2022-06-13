a9190df63ab07612135cdd0a17c5fa2d5bdcc827
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2022-06-13 08:26:56 -0300

    Add files via upload

---
+++ atividade71.py
@@ -0,0 +1,1 @@
+# desafio 67 - Faça um programa que mostre a tabuada de varios números. um de cada vez, para cada valor digitado pelo usuario. o programa será interrompido quando o número solicitado for negativo.
---
+++ atividade75.py
@@ -0,0 +1,2 @@
+# desafio 71 - Crie um programa que simule o funcionamento de um caixa eletronico. no inicio, pergunte oa usuario qual será o valor a ser sacado (numero inteiro) e o quantas cedulas de cada valor serão entegues.
+# obs: considere que o caixa possui cedulas de R$50, R$20, R$10 e R$1.
