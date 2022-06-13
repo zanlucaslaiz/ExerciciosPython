99bfd01467d90bee85b8987744ee5d204a8b956c
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2022-06-13 08:28:46 -0300

    Delete atividade73.py

--- atividade73.py
+++
@@ -1,4 +0,0 @@
-# desafio 69 - crie um programa que leia a idade o sexo de varias pessoas. a cada pessoa cadastrada. o programa deverá perguntar se o usuario quer ou não continuar. no final mostre: 
-# a - quantas pessoas tem mais de 18 anos.
-# b - quantos homens foram cadastrados.
-# c - quantas mulheres tem menos de 20 anos.
