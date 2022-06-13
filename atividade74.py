05deed2ac192da65f3d2edaa454ccca5d9653b23
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2022-06-13 08:28:58 -0300

    Delete atividade74.py

--- atividade74.py
+++
@@ -1,4 +0,0 @@
-# desafio 70 - Crie um programa que leia o nome e o preço de varios produtos. O programa deverá perguntar se o usuario vai continar. No final, mostre:
-# A- qual é o total gasto na comprar. 
-# B- quantos produtos custam mais de R$ 1000. 
-# C- qual é o nome do produto mais barato. 
